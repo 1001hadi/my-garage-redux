b76a32678c1221c110b47432f5bef97c8a69dda7
1001hadi <hadi.jaf.1001@gmail.com>
2024-01-06 14:02:25 -0700

    search functionality has been added

--- src/componenets/CarValue.js
+++ src/componenets/CarValue.js
@@ -1,7 +1,14 @@
-import React from "react";
+import { useSelector } from "react-redux";
 
 function CarValue() {
-  return <div>CarValue</div>;
+  const totalCost = useSelector(({ cars: { data, searchTerm } }) => {
+    return data
+      .filter((car) => {
+        return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+      })
+      .reduce((total, car) => total + car.cost, 0);
+  });
+  return <div className="car-value">Total Cost: ${totalCost}</div>;
 }
 
 export default CarValue;
